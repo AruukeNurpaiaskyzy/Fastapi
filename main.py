4f7090d81a1a78e97cc24e8c8880e73418fd3b35
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-08-04 03:31:11 +0600

    mini project related with the books

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import uvicorn
 
 app = FastAPI()
-@app.get(path="/", summary ="Главная ручка", tags=['основные ручки'])
-def root():
-    return"Hello world Aru"
+# @app.get(path="/", summary ="Главная ручка", tags=['основные ручки'])
+# def root():
+#     return"Hello world Aru"
+
+# if __name__ == "__main__":
+#     uvicorn.run("main.app", )
+
+books = [
+    {
+        "id":1,
+        "title": "Асинрхоронность в пайтон",
+        "author": "Мэтью",
+    },
+    {
+        "id":2,
+        "title":"backend разработка пайтон",
+        "author": "Артем",
+    },
+]
+
+@app.get("/books")
+def read_books():
+    return books
+
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Книга не найдена")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main.app", reload=True)
