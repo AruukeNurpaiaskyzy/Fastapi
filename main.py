9fb0a9e4733281f2c4732895cbec898f962fcf28
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-08-05 22:12:37 +0600

    dgh

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ books = [
 def read_books():
     return books
 
+@app.get("/books/{book_id}", tags=["Книги"], summary="Получить конкретную книгу")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Книга не найдена")
 
 @app.get("/books/{book_id}")
 def get_book(book_id: int):
